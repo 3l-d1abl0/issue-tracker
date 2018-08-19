a2fa47548f34f70e2d7c59027927d7e7609d5f9e
Sameer Barha <sameer.barha12@gmail.com>
2018-08-19 16:12:59 +0530

    added exception to email send

--- new_celery/tasks.py
+++ new_celery/tasks.py
@@ -3,18 +3,15 @@ from new_celery.celery import app
 from new_celery.email import send_email
 from celery.utils.log import get_task_logger
 import time
+from logger import logger
 
 #logger = get_task_logger(__name__)
 
 @app.task(name="send_email_task")
 def send_email_task(email_address, subject, body):
     #logger.info("Sening Email")
-    return send_email(email_address, subject, body)
-'''
-def longtime_add(x, y):
-    print 'long time task begins'
-    # sleep 5 seconds
-    time.sleep(5)
-    print 'long time task finished'
-    return x + y
-'''
+    try:
+        return send_email(email_address, subject, body)
+    except Exception as e:
+        logger.debug(e)
+        return e
